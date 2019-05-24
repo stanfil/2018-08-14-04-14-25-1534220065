067f917c0c950148671788c6fb62a903c1320bdc
stanfil <352070351@qq.com>
2019-05-24 13:25:28 +0800

    js2 complete

--- main/main.js
+++ main/main.js
@@ -1,4 +1,25 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input) {
+    // console.log("Debug Info");
+    // return 'Hello World!';
+    let n = [[],[],[]]
+    let str = 
+    `._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|`
+    str.split('\n').forEach((a, i)=>{
+        a.split('   ').forEach((b, j)=>{
+            n[i][j] = b
+        })
+    })
+    let ans = [[],[],[]]
+    input.split('').forEach((a, i)=>{
+        ans[0][i] = n[0][a]
+        ans[1][i] = n[1][a]
+        ans[2][i] = n[2][a]
+        
+    })
+    ans = ans.map(item => item.join(' '))
+    let output = ans.join('\n')+'\n'
+    console.log(output)
+    return output
 };
